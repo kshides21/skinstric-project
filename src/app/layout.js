29d3c7e25d0b4eb62e04cbcd303da16c50157921
kshides21 <shidelerk@gmail.com>
2025-11-14 14:13:22 -0500

    made home page with transitions

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,19 @@
-import "./globals.css";
+import localFont from "next/font/local";
+
+const roobert = localFont({
+  src: [
+    {
+      path: "../../public/fonts/roobert/Roobert-Regular.woff2",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/roobert/Roobert-Medium.woff2",
+      weight: "500",
+      style: "normal",
+    },
+  ],
+});
 
 export const metadata = {
   title: "Skinstric",
@@ -8,9 +23,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`antialiased font-roobert`}
-      >
+      <body className={roobert.className}>
         {children}
       </body>
     </html>
--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import discover from "../../assets/discover.svg";
+import test from "../../assets/take-test.svg";
 import rectagle from "../../assets/Rectangle.svg";
 import Image from "next/image";
 import styles from "./page.module.css";
@@ -9,7 +10,7 @@ export default function Home() {
     <>
       <header className={styles.header}>
         <div className={styles.header__home__left}>
-          <Link href={"/"}>
+          <Link className={styles.header__link__wrapper} href={"/"}>
             <div className={styles.header__link}>SKINSTRIC</div>
           </Link>
           <div className={styles.header__sublink}>{`[ intro ]`}</div>
@@ -18,24 +19,46 @@ export default function Home() {
       </header>
       <main>
         <div className={styles.home__intro__layout}>
-          <div>
+          <div className={styles.leftSide}>
             <div>
-              <Image className={styles.rectagle} src={rectagle} alt="outline" />
+              <Image
+                className={styles.rectangle}
+                src={rectagle}
+                alt="outline"
+              />
             </div>
-            <div className={styles.discover}>
-              <Image src={discover} alt="DISCOVER A.I." />
+            <div className={styles.discover__wrapper}>
+              <button className={styles.discover__btn}>
+                <div className={styles.discover}>
+                  <Image src={discover} alt="DISCOVER A.I." />
+                </div>
+              </button>
             </div>
           </div>
-          <div className={styles.home__title}>Sophistocated skincare</div>
-          <div>
-            <div className={styles.rectagle}>
+
+          <div className={styles.titleWrapper}>
+            <h1 className={styles.home__title}>
+              Sophistocated <br /> skincare
+            </h1>
+          </div>
+
+          <div className={styles.rightSide}>
+            <div className={styles.rectangle__right}>
               <Image src={rectagle} alt="outline" />
             </div>
-            <div className={styles.discover}>
-              <Image src={discover} alt="DISCOVER A.I." />
+            <div className={styles.test__wrapper}>
+              <button className={styles.test__btn}>
+                <div className={styles.test__normal}>
+                  <Image src={test} alt="take test" />
+                </div>
+              </button>
             </div>
           </div>
         </div>
+        <p className={styles.title__text}>
+          SKINSTRIC DEVELOPED AN A.I. THAT CREATES A HIGHLY-PERSONALIZED ROUTINE
+          TAILORED TO WHAT YOUR SKIN NEEDS.
+        </p>
       </main>
     </>
   );
