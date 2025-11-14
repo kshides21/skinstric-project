334500877e472a9df2ae0ac89f1fbe42b9b79e2f
kshides21 <shidelerk@gmail.com>
2025-11-13 22:05:42 -0500

    started home page, starting to lay down elements

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,42 @@
+import Link from "next/link";
+import discover from "../../assets/discover.svg";
+import rectagle from "../../assets/Rectangle.svg";
+import Image from "next/image";
+import styles from "./page.module.css";
+
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      Testing
-    </div>
+    <>
+      <header className={styles.header}>
+        <div className={styles.header__home__left}>
+          <Link href={"/"}>
+            <div className={styles.header__link}>SKINSTRIC</div>
+          </Link>
+          <div className={styles.header__sublink}>{`[ intro ]`}</div>
+        </div>
+        <div className={styles.header__home__right}>ENTER CODE</div>
+      </header>
+      <main>
+        <div className={styles.home__intro__layout}>
+          <div>
+            <div>
+              <Image className={styles.rectagle} src={rectagle} alt="outline" />
+            </div>
+            <div className={styles.discover}>
+              <Image src={discover} alt="DISCOVER A.I." />
+            </div>
+          </div>
+          <div className={styles.home__title}>Sophistocated skincare</div>
+          <div>
+            <div className={styles.rectagle}>
+              <Image src={rectagle} alt="outline" />
+            </div>
+            <div className={styles.discover}>
+              <Image src={discover} alt="DISCOVER A.I." />
+            </div>
+          </div>
+        </div>
+      </main>
+    </>
   );
 }
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+export default {
+  content: [
+    "./src/app/**/*.{js,jsx}",
+    "./src/components/**/*.{js,jsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        roobert: ["Roobert", "sans-serif"],
+      },
+    },
+  },
+};
